dcfe4807060851583990cdd6a2fbeed2d5cee97a
FitzWM <fitzwm@protonmail.com>
2021-05-20 14:31:33 -0500

    Added simple setup script

---
+++ home/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd
+
+# Set up Zsh
+ln -s ~/.dot/home/.zshrc .zshrc
+ln -s ~/.dot/home/.zsh .zsh
+
+# Set up Vim and backup directory.
+ln -s ~/.dot/home/.vimrc .vimrc
+mkdir -P .vim/tmp
+
+# Change default shell and reload.
+chsh -s /bin/zsh
+exec zsh
+
+exit
