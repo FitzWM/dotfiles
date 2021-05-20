16c73ae91f1f14f10c1c88177536c02449623711
FitzWM <fitzwm@protonmail.com>
2021-05-20 14:35:16 -0500

    Corrected setup script

--- home/setup.sh
+++ home/setup.sh
@@ -3,11 +3,11 @@
 cd
 
 # Set up Zsh
-ln -s ~/.dot/home/.zshrc .zshrc
+ln -s ~/.dot/home/zshrc .zshrc
 ln -s ~/.dot/home/.zsh .zsh
 
 # Set up Vim and backup directory.
-ln -s ~/.dot/home/.vimrc .vimrc
+ln -s ~/.dot/home/vimrc .vimrc
 mkdir -P .vim/tmp
 
 # Change default shell and reload.
