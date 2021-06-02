227a23958678661b04218ebcb452cca958b9bdbd
Fitzwilliam Penn <fitz@localhost.localdomain>
2021-06-02 17:32:46 +0000

    Corrected mkdir commands.

--- home/setup.sh
+++ home/setup.sh
@@ -5,11 +5,11 @@ cd
 # Set up Zsh
 ln -s ~/.dot/home/zshrc .zshrc
 ln -s ~/.dot/home/.zsh .zsh
-mkdir -P ~/.cache/zsh
+mkdir -p ~/.cache/zsh
 
 # Set up Vim and backup directory.
 ln -s ~/.dot/home/vimrc .vimrc
-mkdir -P .vim/tmp
+mkdir -p .vim/tmp
 
 # Change default shell and reload.
 chsh -s /bin/zsh
